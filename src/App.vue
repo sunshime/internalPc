e39bb9d9f3e28c119471f758899f95e6f40aeabf
lfj <liao_fengji@163.com>
2019-08-30 18:34:54 +0800

    开始实现登录页面

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
--- src/page/Login.vue
+++ src/page/Login.vue
@@ -1,5 +1,33 @@
 <template>
-    <div class="login">
-        这是登录页面
+  <div class="login">
+    <div class="container clearfix">
+        <div class="login-icon fl">
+            <img src="../common/images/login-form.jpg" alt="">
+        </div>
+      <el-form ref="form" :model="form" class="lg-form fr">
+        <el-form-item label="用户名">
+          <el-input v-model="form.userName"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="form.userPwd"></el-input>
+        </el-form-item>
+        <el-button type="primary">登录</el-button>
+      </el-form>
     </div>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+        form:{
+            userName:'',
+            userPwd:'',
+        }
+    };
+  }
+};
+</script>
+<style lang="css" scoped>
+@import url("../common/style/pc-css.css");
+</style>
